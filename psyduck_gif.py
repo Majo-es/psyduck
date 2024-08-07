88013dc07a1313beb3a1a90dee7388d8c7136864
Maria Jose <43044338+Majo-es@users.noreply.github.com>
2024-08-06 23:01:39 -0600

    Update psyduck_gif.py

--- psyduck_gif.py
+++ psyduck_gif.py
@@ -4,9 +4,4 @@ image_path_list = ['psyduck1.jpeg', 'psyduck2.jpeg', 'psyduck3.jpeg']
 
 image_list = [Image.open(file) for file in image_path_list]
 
-image_list[0].save(
-            #'animation.gif',
-            #save_all=True,
-           # append_images=image_list[1:], 
-           # duration=800, 
-           # loop=4)
+image_list[0].save('animation.gif',save_all=True, append_images=image_list[1:], duration=800, loop=0)
